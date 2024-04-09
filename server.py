1740b833a34e6045b39e771119c290d6c493fbb6
User1 <nikitas3n@gmail.com>
2024-04-09 19:07:15 +0300

    Добавлена ассинхронность

--- server.py
+++ server.py
@@ -3,15 +3,21 @@ from main import get_token, auth, get_message_history
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import asyncio
 import json
+import time
+
 app = Flask(__name__)
 response = get_token(auth)
 lock = asyncio.Lock()
 
 if response != 1:
     giga_token = response.json()['access_token']
+    expires_at = response.json()['expires_at']
 
-@app.route('/send_to_GigaChat/<message>', methods=['POST'])
+@app.route('/send_to_GigaChat/<message>', methods=['POST', 'GET'])
 async def give_greeting(message):
+    if round(time.time()*1000) >= expires_at:
+        async with lock:
+            refresh()
     if request.data != b"":
         conversation_history = str(request.data, "utf-8")
         conversation_history = json.loads(conversation_history.replace("\n", ""))
@@ -21,18 +27,14 @@ async def give_greeting(message):
     response_data = response.json()
     return response_data['choices'][0]['message']['content']
 
-async def refresh():
-    global giga_token
-    async with lock:
-        print("refresh")
-        response = get_token(auth)
-        if response != 1:
-            giga_token = response.json()['access_token']
-async def main():
-    scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
-    scheduler.start()
-    scheduler.add_job(refresh, 'interval', minutes=1)
-    app.run(host='0.0.0.0', port=4567)
+def refresh():
+    global giga_token, expires_at
+    response = get_token(auth)
+    if response != 1:
+        giga_token = response.json()['access_token']
+        expires_at = response.json()['expires_at']
+    return
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    app.run(host='0.0.0.0', port=4567)
