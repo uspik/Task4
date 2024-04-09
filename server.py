a6b0899c9aeaede2dcebed3c2bb2232987502143
User1 <nikitas3n@gmail.com>
2024-04-09 18:56:22 +0300

    Добавлена ассинхронность

--- server.py
+++ server.py
@@ -12,20 +12,27 @@ if response != 1:
 
 @app.route('/send_to_GigaChat/<message>', methods=['POST'])
 async def give_greeting(message):
-    conversation_history = str(request.data, "utf-8")
-    response, conversation_history = await get_message_history(giga_token, message, json.loads(conversation_history.replace("\n", "")))
+    if request.data != b"":
+        conversation_history = str(request.data, "utf-8")
+        conversation_history = json.loads(conversation_history.replace("\n", ""))
+    else:
+        conversation_history = None
+    response, conversation_history = await get_message_history(giga_token, message, conversation_history)
     response_data = response.json()
     return response_data['choices'][0]['message']['content']
 
 async def refresh():
     global giga_token
     async with lock:
+        print("refresh")
         response = get_token(auth)
         if response != 1:
             giga_token = response.json()['access_token']
-
-if __name__ == "__main__":
+async def main():
     scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
     scheduler.start()
-    scheduler.add_job(refresh, 'interval', minutes=29)
+    scheduler.add_job(refresh, 'interval', minutes=1)
     app.run(host='0.0.0.0', port=4567)
+
+if __name__ == "__main__":
+    asyncio.run(main())
